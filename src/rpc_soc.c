f17b440480037ceffce98989c22d454ada807f81
Chuck Lever <chuck.lever@oracle.com>
2015-06-10 14:12:53 -0400

    Revert commit c0547c56dafb ('Add configure option to disable DES')
    AUTH_DES is part of the legacy SunRPC API in glibc, and therefore
    is not optional.
    
    Commit c0547c56dafb says making AUTH_DES optional "is needed for
    musl libc." Removing AUTH_DES breaks legacy code, thus some other
    fix must be found for this issue. libtirpc can provide some or all
    of the missing functionality if the local C library does not have
    it, perhaps.
    
    Reported-by: Thorsten Kukuk <kukuk@suse.de>
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>

--- src/rpc_soc.c
+++ src/rpc_soc.c
@@ -520,7 +520,6 @@ clnt_broadcast(prog, vers, proc, xargs, argsp, xresults, resultsp, eachresult)
 	    (resultproc_t) rpc_wrap_bcast, "udp");
 }
 
-#if HAVE_AUTHDES
 /*
  * Create the client des authentication object. Obsoleted by
  * authdes_seccreate().
@@ -552,7 +551,6 @@ fallback:
 	dummy = authdes_seccreate(servername, window, NULL, ckey);
 	return (dummy);
 }
-#endif
 
 /*
  * Create a client handle for a unix connection. Obsoleted by clnt_vc_create()
