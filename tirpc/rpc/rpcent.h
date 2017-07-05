e45bf420983e834003f673188d8f4688494978a8
Joshua Kinard <kumba@gentoo.org>
2017-07-05 10:54:48 -0400

    Fix struct rpcent for uclibc-ng
    Commit 6d8d4b5a7bf6 addresses an issue for musl, based on a description of
    an earlier patch at https://patchwork.kernel.org/patch/5499671/.  That
    description notes uncertainty with uclibc, which also defines __GLIBC__.
    This patch fixes the uclibc case by also checking for __UCLIBC__.
    
    Fixes: 6d8d4b5a7bf6 ("Include string.h for memset")
    Signed-off-by: Joshua Kinard <kumba@gentoo.org>
    Signed-off-by: Steve Dickson <steved@redhat.com>
    Cc: Natanael Copa <ncopa@alpinelinux.org>

--- tirpc/rpc/rpcent.h
+++ tirpc/rpc/rpcent.h
@@ -49,7 +49,7 @@ extern "C" {
 #endif
 
 /* These are defined in /usr/include/rpc/netdb.h */
-#if !defined(__GLIBC__)
+#if !defined(__GLIBC__) || defined(__UCLIBC__)
 struct rpcent {
 	char	*r_name;	/* name of server for this rpc program */
 	char	**r_aliases;	/* alias list */
