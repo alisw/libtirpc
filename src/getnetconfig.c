a1fae253053bde4786a40c7ac1ba05f53d78398f
Steve Dickson <steved@redhat.com>
2018-09-07 12:56:00 -0400

    getnetconfig.c: cppcheck_warning
    Memory leak: p
    Memory leak: tmp
    
    Signed-off-by: Steve Dickson <steved@redhat.com>

--- src/getnetconfig.c
+++ src/getnetconfig.c
@@ -709,6 +709,8 @@ struct netconfig	*ncp;
     p->nc_lookups = (char **)malloc((size_t)(p->nc_nlookups+1) * sizeof(char *));
     if (p->nc_lookups == NULL) {
 	free(p->nc_netid);
+	free(p);
+	free(tmp);
 	return(NULL);
     }
     for (i=0; i < p->nc_nlookups; i++) {
