linuxbox2/ntirpc

commit 6d8d4b5a7bf63812b95d152762c604cdc1ee3911

Include string.h for memset
This fixes warning: implicit declaration of function 'memset'

Signed-off-by: Natanael Copa <ncopa@alpinelinux.org>
Signed-off-by: Steve Dickson <steved@redhat.com>

tirpc/rpc/rpcent.h
@@ -49,7 +49,7 @@ extern "C" {
 #endif
 
 /* These are defined in /usr/include/rpc/netdb.h */
-#if 0
+#if !defined(__GLIBC__)
 struct rpcent {
 	char	*r_name;	/* name of server for this rpc program */
 	char	**r_aliases;	/* alias list */

src/authgss_prot.c
@@ -37,6 +37,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <string.h>
 #include <rpc/types.h>
 #include <rpc/xdr.h>
 #include <rpc/auth.h>

alisw/libtirpc

commit 83b7ac7f3dc74ae021903c4146e48823147a2e32

Commit e45bf420983e fixed issue for uclibc-ng, but on uclibc this depends on __UCLIBC_HAS_RPC__.
This is old patch from Buildroot commit c54af0a294 ("libtirpc: handle
the case where uClibc may have RPC support") by Thomas Petazzoni.

Fixes: 6d8d4b5a7bf6 ("Include string.h for memset")
Fixes: e45bf420983e ("Fix struct rpcent for uclibc-ng")

Signed-off-by: Petr Vorel <petr.vorel@gmail.com>
Signed-off-by: Steve Dickson <steved@redhat.com>

tirpc/rpc/rpcent.h
@@ -48,8 +48,9 @@
 extern "C" {
 #endif
 
-/* These are defined in /usr/include/rpc/netdb.h */
-#if !defined(__GLIBC__) || defined(__UCLIBC__)
+/* These are defined in /usr/include/rpc/netdb.h, unless we are using
+   the C library without RPC support. */
+#if defined(__UCLIBC__) && !defined(__UCLIBC_HAS_RPC__) || !defined(__GLIBC__)
 struct rpcent {
 	char	*r_name;	/* name of server for this rpc program */
 	char	**r_aliases;	/* alias list */